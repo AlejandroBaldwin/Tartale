fbd6264675db4389546c53a47d903e62642be566
Alejandro-Baldwin <alejandro.baldwin98@gmail.com>
2020-05-24 18:29:01 -0400

    Updates

---
+++ precache-manifest.c71bc7f60580b852f6faee3de014d669.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d3e2099abe5c16fc37f59496c1eeea",
+    "url": "/Tartale/index.html"
+  },
+  {
+    "revision": "817bb6b4008a95e99804",
+    "url": "/Tartale/static/css/main.98978e65.chunk.css"
+  },
+  {
+    "revision": "6d0f362b0c655c6d7ff3",
+    "url": "/Tartale/static/js/2.e7e7b328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tartale/static/js/2.e7e7b328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817bb6b4008a95e99804",
+    "url": "/Tartale/static/js/main.f375b94d.chunk.js"
+  },
+  {
+    "revision": "5969119218501b69a3a0",
+    "url": "/Tartale/static/js/runtime-main.b10f9a7c.js"
+  },
+  {
+    "revision": "c5adbdb895473729c91c1f9651bf79d5",
+    "url": "/Tartale/static/media/alfajor.c5adbdb8.jpeg"
+  },
+  {
+    "revision": "1e81b16d7fe51651eda5a42e370d02b1",
+    "url": "/Tartale/static/media/brownies.1e81b16d.jpeg"
+  },
+  {
+    "revision": "d7d15449271d234b545bcad089dfdcd3",
+    "url": "/Tartale/static/media/crema_volteada.d7d15449.jpeg"
+  },
+  {
+    "revision": "626357abcd4f391c15f38a1f029cbcac",
+    "url": "/Tartale/static/media/logo.626357ab.jpeg"
+  },
+  {
+    "revision": "ed9031bdce42c00ce5caaa33058973a0",
+    "url": "/Tartale/static/media/mango_pie.ed9031bd.jpeg"
+  },
+  {
+    "revision": "a4949e987d4abed73309850ebb305e57",
+    "url": "/Tartale/static/media/mini_cake.a4949e98.jpeg"
+  },
+  {
+    "revision": "a5ceab0db0e9e9f755fd48601338cf0c",
+    "url": "/Tartale/static/media/mini_pie.a5ceab0d.jpeg"
+  }
+]);
