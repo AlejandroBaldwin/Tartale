5e1ad927599b18c641dfb365476a181bc29cbb4b
Alejandro-Baldwin <alejandro.baldwin98@gmail.com>
2020-06-12 23:02:21 -0400

    Quick fix on the modal height

--- src/components/DessertDescription/DessertDescription.js
+++ src/components/DessertDescription/DessertDescription.js
@@ -8,7 +8,7 @@ const DessertDescription = ({ src, id, alt, modalState, description, changeRoute
             <section className='dessert-description__container__contents'>
                 <h1>{description}</h1>
                 <img className='dessert-description__container__img' src={src} id={id} key={id} alt={alt} />
-                <p className='dessert-description__container__description'>Veniam ipsum enim aute et voluptate ad nulla consequat anim. Incididunt pariatur mollit amet consequat tempor elit occaecat eu. Magna deserunt aliqua laborum enim duis dolore aliquip dolore quis minim. Ad adipisicing ad et commodo ullamco ex veniam consequat minim esse et excepteur. Deserunt amet esse duis veniam.</p>
+                <p className='dessert-description__container__description'>Veniam ipsum enim aute et voluptate ad nulla consequat anim. Incididunt pariatur mollit amet consequat tempor elit occaecat eu. Magna deserunt aliqua laborum  veniam.</p>
                 <section className='dessert-description__container__CTA'><Button id='contact' content='Haz tu Pedido!' changeRoute={changeRoute} /></section>
             </section>
         </section>
